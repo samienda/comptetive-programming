1b2d1b34883a362a8ab142cab4bf4fb870d6c745
samienda <134318401+samienda@users.noreply.github.com>
2024-01-26 21:33:30 +0300

    Added solution for Minimum Window Substring

--- leetcode-solutions/minimum-window-substring.py
+++ leetcode-solutions/minimum-window-substring.py
@@ -6,31 +6,29 @@ class Solution:
         have = Counter()
         k = len(need)
         count = n + 1
-        res = ""
+        res = [-1, -1]
         check = set()
 
         left = 0
         for right in range(n):
             have[s[right]] += 1
 
-            if s[right] in need and s[right] not in check and have[s[right]] >= need[s[right]]:
-                check.add(s[right])
+            if s[right] in need and have[s[right]] == need[s[right]]:
                 k -= 1
 
             while k <= 0:
                 if count > right - left + 1:
                     count =  right - left + 1
-                    res = s[left: right + 1]
+                    res = [left, right + 1]
                 
                 have[s[left]] -= 1
-                if s[left] in need and s[left] in check and have[s[left]] < need[s[left]]:
+                if s[left] in need and have[s[left]] < need[s[left]]:
                     k += 1
-                    check.remove(s[left])
-
+                    
 
 
                 left += 1 
 
         
-        return res
+        return s[res[0]: res[1]]
         
