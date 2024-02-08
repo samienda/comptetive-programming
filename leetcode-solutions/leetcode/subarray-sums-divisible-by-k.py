3c80abda204c7505dc12d840af7f6e7add30c374
samienda <134318401+samienda@users.noreply.github.com>
2024-02-08 14:39:22 +0300

    Add solution for Subarray Sums Divisible by K

--- leetcode-solutions/leetcode/subarray-sums-divisible-by-k.py
+++ leetcode-solutions/leetcode/subarray-sums-divisible-by-k.py
@@ -2,22 +2,18 @@ class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         n = len(nums)
         # nums.insert(0, 0)
-        freq = Counter([0])
+        freq = [1] + [0] * k
 
         count = 0
         running = 0
-        for i in range(n):
-            running += nums[i]
-            nums[i] = running / k
-            remainder = round(nums[i] % 1, 5)
+        for num in nums:
+            running = (num + running) % k
+             
+            count += freq[running]
 
-            if remainder in freq:
-                count += freq[remainder]
-
-            freq[remainder] += 1
+            freq[running] += 1
 
         
-        print(nums)
         # for r in range(1, n + 1):
         #     for l in range(r):
         #         curr = nums[r] - nums[l]
@@ -26,4 +22,3 @@ class Solution:
 
 
         return count
-        
