97e74ab8787ac9d8274266399e47bb949900c4f8
samienda <134318401+samienda@users.noreply.github.com>
2024-02-19 14:29:48 +0300

    Add solution for Implement Stack using Queues

---
+++ leetcode-solutions/leetcode/implement-stack-using-queues.py
@@ -0,0 +1,37 @@
+class MyStack:
+
+    def __init__(self):
+        self.queue = deque()
+
+        
+
+    def push(self, x: int) -> None:
+        queue = deque() 
+        while self.queue:
+            queue.append(self.queue.popleft())
+
+        self.queue.append(x)
+
+        while queue:
+            self.queue.append(queue.popleft())
+            
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+        
+
+    def top(self) -> int:
+        return self.queue[0]
+        
+
+    def empty(self) -> bool:
+        return not self.queue
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
