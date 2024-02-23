876b37317c9150c1a4e960174c655b8367559320
samienda <134318401+samienda@users.noreply.github.com>
2024-02-23 15:32:48 +0300

    Add solution for Kth Smallest Element in a BST

---
+++ leetcode-solutions/leetcode/kth-smallest-element-in-a-bst.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        res = []
+        def inorder(root):
+            nonlocal res
+            if not root:
+                return root
+
+
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+
+        inorder(root)
+        return res[k - 1]
+
+
