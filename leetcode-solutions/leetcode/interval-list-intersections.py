e62bae8e6f56caf612f32299605ac521af858a79
samienda <134318401+samienda@users.noreply.github.com>
2024-03-01 10:11:58 +0300

    Add solution for Interval List Intersections

---
+++ leetcode-solutions/leetcode/interval-list-intersections.py
@@ -0,0 +1,28 @@
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        
+
+        final = firstList + secondList
+        final.sort(key=lambda item:item[1])
+        final.sort()
+
+        last = final[0][1]
+        res = []
+        # stack = []
+
+        for start, end in final[1:]:
+            # if stack and stack[-1] > start:
+            #     res.append([start, stack.pop()])
+            if end < last:
+                res.append([start, end])
+                continue
+            elif start <= last:
+                res.append([start, last])
+            elif end <= last:
+                res.append(start, end)
+            last = end
+        # print(stack)
+
+
+        # print(final)
+        return res
