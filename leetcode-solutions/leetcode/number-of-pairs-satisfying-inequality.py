29121d64a55abf48802cea85e4b30055e3768852
samienda <134318401+samienda@users.noreply.github.com>
2024-03-20 23:54:49 +0300

    Add solution for Number of Pairs Satisfying Inequality

--- leetcode-solutions/leetcode/number-of-pairs-satisfying-inequality.py
+++ leetcode-solutions/leetcode/number-of-pairs-satisfying-inequality.py
@@ -1,15 +1,72 @@
 class Solution:
     def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
-        n = len(nums1)
+        def merge(lefthalf, righthalf):
 
-        memo = deque()
-        count = 0
+            left = 0
+            right = 0
+            count = 0
+            curr = 0
+
+            newlis = []
+            queue = deque(lefthalf)
+            while left < len(lefthalf) and right < len(righthalf):
+                # print(lefthalf, righthalf, curr, count, newlis, queue)
+
+                if lefthalf[left] > righthalf[right]:
+                    newlis.append(righthalf[right])
+
+                    while queue and queue[0] <= righthalf[right] + diff:
+                        curr += 1
+                        queue.popleft()
+
+                    count += curr
+                    right += 1
+                else:
+                    newlis.append(lefthalf[left])
+                    left += 1
+
+            while left < len(lefthalf):
+                # print(lefthalf, righthalf, curr, count, newlis, queue)
+                newlis.append(lefthalf[left])
+                left += 1
+                    
+           
+            while right < len(righthalf):
+                # print(lefthalf, righthalf, curr, count, newlis, queue)
+                newlis.append(righthalf[right])
+                
+                while queue and queue[0] <= righthalf[right] + diff:
+                    queue.popleft()
+                    curr += 1
+
+                count += curr
+                right += 1
+
+            # print(lefthalf, righthalf, curr, count, newlis, queue)
+            return newlis, count
+                
+        
+        def divide(left, right, nums):
+            if left == right:
+                return [nums[left]], 0
+
+            mid = (left + right) // 2
+
+            lefthalf, leftcount = divide(left, mid, nums)
+            righthalf, rightcount = divide(mid + 1, right, nums)
+
+            combined, combinedcount = merge(lefthalf, righthalf)
+            return combined, leftcount + combinedcount + rightcount
+
+
+        n = len(nums1)
+        nums = [0] * n
         for i in range(n): # O(n)
 
-            store = nums1[i] - nums2[i]
-            check = nums1[i] - nums2[i] + diff
+            nums[i] = nums1[i] - nums2[i]
 
-            count += bisect.bisect_right(memo, check) # O(logn)
-            memo.insert(bisect.bisect_right(memo, store), store) # O(logn)
 
+        # print(nums)
+        nums, count = divide(0, n - 1, nums)
+        # print(nums, count)
         return count
