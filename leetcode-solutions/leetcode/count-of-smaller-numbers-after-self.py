60665578b89a47dab85e6b94f71a6e7d10bb9611
samienda <134318401+samienda@users.noreply.github.com>
2024-03-20 23:55:27 +0300

    Add solution for Count of Smaller Numbers After Self

--- leetcode-solutions/leetcode/count-of-smaller-numbers-after-self.py
+++ leetcode-solutions/leetcode/count-of-smaller-numbers-after-self.py
@@ -1,21 +1,52 @@
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
         n = len(nums)
+        nums = [ (i, num) for i, num  in enumerate(nums)]
 
-        memo = deque()
+        def merge(lefthalf, righthalf):
+            left = 0
+            right = 0
+            newlis = []
+            count = 0
+            # print(memo)
+            while left < len(lefthalf) and right < len(righthalf):
+                if lefthalf[left][1] > righthalf[right][1]:
+                    count += 1
+                    newlis.append(righthalf[right])
+                    right += 1
+                else:
+                    memo[lefthalf[left][0]] += count
+                    newlis.append(lefthalf[left])
+                    left += 1
+
+            while left < len(lefthalf):
+                memo[lefthalf[left][0]] += count
+                newlis.append(lefthalf[left])
+                left += 1
+
+            while right < len(righthalf):
+                newlis.append(righthalf[right])
+                right += 1
+
+            return newlis
+
+
+        def divide(left, right, nums):
+            if left == right:
+                return [nums[left]]
+
+            mid = (left + right) // 2
+            
+            return merge(divide(left, mid, nums), divide(mid + 1, right, nums))
+
+        memo = defaultdict(int)
+        divide(0, n - 1, nums)
+
         ans = [0] * n
-
-        for i in range(n - 1, -1, -1):
-
-            idx = bisect.bisect_left(memo, nums[i])
-            ans[i] = idx
-            memo.insert(idx, nums[i])
-
+        for idx, value in memo.items():
+            ans[idx] = memo[idx]
+        # print(memo)
         return ans
-
-
-
-
-
+            
 
         
