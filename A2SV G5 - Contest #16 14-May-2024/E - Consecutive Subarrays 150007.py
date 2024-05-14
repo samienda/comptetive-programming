f793f9730a905958c26fe3f618d6aab8a9abca8f
samienda <134318401+samienda@users.noreply.github.com>
2024-05-14 19:36:10 +0300

    Created 'E - Consecutive Subarrays' by A2SV Hub

---
+++ A2SV G5 - Contest #16 14-May-2024/E - Consecutive Subarrays 150007.py
@@ -0,0 +1,13 @@
+# Problem: E - Consecutive Subarrays - https://codeforces.com/gym/523525/problem/E
+
+n, k = list(map(int, input().split()))
+array = list(map(int, input().split()))
+
+suffix = array[:]
+for i in range(n - 2, -1, -1):
+    suffix[i] += suffix[i + 1]
+
+
+ans = suffix[0] + sum(sorted(suffix[1:], reverse=True)[:k - 1])
+
+print(ans)
